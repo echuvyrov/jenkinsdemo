394981c1141121efd320560797d0cae5a7d4533a
Eugene Chuvyrov <eugene.chuvyrov@microsoft.com>
2018-06-15 02:46:13 -0700

    Added MSI

--- infrastructure/terraform_resources/main.tf
+++ infrastructure/terraform_resources/main.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
-  version = "=1.4.0"
+  version      = ">=1.4.0"
+  use_msi      = "true"
+  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
 }
 
 resource "azurerm_resource_group" "jenkinstest" {
