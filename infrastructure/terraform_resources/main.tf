1c1b6adf958b80f6e4e47b07ae4be857db92f4e2
Eugene Chuvyrov <eugene.chuvyrov@microsoft.com>
2018-06-15 03:18:18 -0700

    Removed RG

--- infrastructure/terraform_resources/main.tf
+++ infrastructure/terraform_resources/main.tf
@@ -1,10 +1,11 @@
 provider "azurerm" {
-  version      = ">=1.4.0"
-  use_msi      = "true"
-  msi_endpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
+  version = ">=1.4.0"
 }
 
+/*
 resource "azurerm_resource_group" "jenkinstest" {
   name     = "jenkinstest"
   location = "westus"
 }
+*/
+
